b281b4f78f24137b3cce1141c25d58215e6d38c3
OverripeWings3 <sjacobust@gmail.com>
2021-04-08 12:06:29 -0500

    Adding Home Endpoint and Menu Route file

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.end('Welcome to the Project!');
 });
 
+app.get('/home', (req, res) => {
+    res.end('home')
+});
+
 app.listen(port, () => {
     console.log(`App is listening in port ${port}`);
 });
